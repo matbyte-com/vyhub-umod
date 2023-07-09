6fb779610be0c6d97ba8812220c2aeb85e007cca
mihailtupickin <bestolo4@bk.ru>
2023-07-09 15:28:22 +0300

    Init Project

---
+++ VyHub.cs
@@ -0,0 +1,41 @@
+﻿namespace Oxide.Plugins;
+
+[Info("VyHub", "VyHub", "1.0.0")]
+public class VyHub : CovalencePlugin
+{
+	#region Fields
+
+	
+
+	#endregion
+
+	#region Config
+
+	
+
+	#endregion
+
+	#region Hooks
+
+	
+
+	#endregion
+
+	#region Interface
+
+	
+
+	#endregion
+
+	#region Utils
+
+	
+
+	#endregion
+
+	#region Lang
+
+	
+
+	#endregion
+}
